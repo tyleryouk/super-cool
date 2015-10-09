57f55566d2a845619909defe889996b85480a16e
Tyler Youk <tyleryouk@tylers-imac.home>
2015-10-09 19:11:48 -0400

    Initial Commit

---
+++ superCool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  superCool
+//
+//  Created by Tyler Youk on 10/9/15.
+//  Copyright © 2015 Tyler Youk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
